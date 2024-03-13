61c7c55d194e43b6313da75d4ce88f9441961510
Diogo Biazus <diogo@biazus.ca>
2024-03-13 10:00:09 -0400

    Decode error message as UTF8 and convert to String when exposing our exception

--- src/Hasql/Notifications.hs
+++ src/Hasql/Notifications.hs
@@ -183,7 +183,7 @@ waitForNotifications sendNotification con =
               result <- PQ.consumeInput pqCon
               unless result $ do
                 mError <- PQ.errorMessage pqCon
-                panic $ maybe "Error checking for PostgreSQL notifications" show mError
+                panic $ maybe "Error checking for PostgreSQL notifications" (T.unpack . T.decodeUtf8) mError
         Just notification ->
           sendNotification (PQ.notifyRelname notification) (PQ.notifyExtra notification)
     panic :: String -> a
